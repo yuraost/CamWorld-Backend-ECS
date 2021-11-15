29ad61944dfa12d3cfb34cd4def72cf9641289b8
Yuriy Ostapchuk <yuraost01@gmail.com>
2021-11-15 15:12:34 +0200

    Dockerize

--- env_vars.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo PORT=${PORT} >> /usr/src/app/.production.env
-echo POSTGRES_HOST=${POSTGRES_HOST} >> /usr/src/app/.production.env
-echo POSTGRES_USER=${POSTGRES_USER} >> /usr/src/app/.production.env
-echo POSTGRES_DB=${POSTGRES_DB} >> /usr/src/app/.production.env
-echo POSTGRESS_PASSWORD=${POSTGRESS_PASSWORD} >> /usr/src/app/.production.env
-echo POSTGRESS_PORT=${POSTGRESS_PORT} >> /usr/src/app/.production.env
-echo PRIVATE_KEY=${PRIVATE_KEY} >> /usr/src/app/.production.env
-exec "$@"
